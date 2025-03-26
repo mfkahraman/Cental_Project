ad0694aeba6753dd5653899b7df4deefe64c2ea9
mfkahraman <mfurkankahraman@gmail.com>
2025-03-26 23:37:17 +0300

    About component completed

---
+++ Cental.WebUI/ViewComponents/Default/_DefaultFactCounterComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultFactCounterComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
