6f196c76d10256a210a7d72af114a6dbae4c77c0
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-04-15 12:13:45 +0300

    All homepage contents are moved to components

---
+++ Cental.WebUI/ViewComponents/Default/_DefaultCarStepsComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.Default
+{
+    public class _DefaultCarStepsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
